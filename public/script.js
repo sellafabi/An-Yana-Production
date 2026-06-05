b5f352c68ef7c7fd37fb5a3a9fd244ef219a6e81
Elle <lr.rfabillar@mmdc.mcl.edu.ph>
2026-06-05 10:08:49 +0800

    Moved website files to root for GitHub Pages

--- public/script.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector("form");
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  alert("Thank you! Your message has been sent.");
-  form.reset();
-});
